5728f8e02cf86447575086c99d69418c9b48ece4
quakex <iquakex@gmail.com>
2017-07-18 17:36:38 +0800

    finish 04 Model 基础学习

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
+  resources :sessions
 
+  namespace :admin do
+    root 'users#index'
+    resources :users do
+      collection do
+        get :search
+      end
+    end
+
+  end
 
 end
